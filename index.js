f1d180aca888073bf63a269809c5e644677ba9b1
KunalNaykode <kunalnaykode@gmail.com>
2024-02-19 22:18:11 +0530

    JavaScirpt Time Updated

--- index.js
+++ index.js
@@ -4,3 +4,14 @@
 let hrs = document.getElementById("hrs");
 let min = document.getElementById("min");
 let sec = document.getElementById("sec");
+
+setInterval(() => {
+  let currentTime = new Date();
+
+  hrs.innerHTML =
+    (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours();
+  min.innerHTML =
+    (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
+  sec.innerHTML =
+    (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
+}, 1000); // 1000ms= 1sec
